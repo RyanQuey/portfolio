6ef61c6394795fe2d0c9cca9ad1cd2a0ce251648
Ryan Quey <quey.ryan@gmail.com>
2020-09-10 05:38:35 +0000

    workable blog implementation

--- src/components/page-components/MediumWidget/MediumWidget.js
+++ src/components/page-components/MediumWidget/MediumWidget.js
@@ -1,6 +1,7 @@
 import React, { useRef, useEffect } from 'react';
 import styles from './MediumWidget.module.scss';
 import Parser from 'rss-parser'
+import moment from 'moment'
 
 // requires an rss feed file there. Right now just manually run scripts/get-blogs-rss.sh
 // import mediumRSS from '../../../utils'
@@ -43,20 +44,28 @@ class MediumWidget extends React.Component {
   render (){
     console.log("current items", this.state.feedItems)
     return(
-      <div className="container">
-        These are my Blog posts
-        {this.state.feedItems.map(item => {
-          return (
-            <div className="card">
-              <img src={item.thumbnail} className="card-img-top post-thumbnail" alt={item.title}></img>
-              <div className="card-body">
-                  <h5 className="card-title post-title">{item.title}</h5>
-                  <p className="card-text post-preview">{item.contentSnippet}</p>
-                  <a href={item.link} className="btn btn-link-grey">Read this article on Medium.com</a>
-            </div>
+      <div id="medium-widget-container" className={styles["container"]}>
+        From time to time I write about what I'm learning or interested in. Check out some of my blog posts here. 
+        <div className={styles["card-container"]}>
+
+          {this.state.feedItems.reverse().map(item => {
+            return (
+              <div className={styles["card"]}>
+                <div className={styles["post-content-preview"]} dangerouslySetInnerHTML={{__html: item.content}}></div>
+                <div className={styles["post-body"]}>
+                  <h5 className={styles["post-title"]}>{item.title}</h5>
+                  <div className={styles["post-subtitle"]}>
+                    {item.contentSnippet.replace("Continue reading on Medium »", "")}
+                  </div>
+                  <div className={styles["date"]}>
+                    {moment(item.isoDate).format("MMMM Do YYYY")}
+                  </div>
+                  <a href={item.link} className={["post-link"]} target="_blank">Read on Medium »</a>
+                </div>
+              </div>
+            )
+          })}
         </div>
-          )
-        })}
       </div>
     )
   }
